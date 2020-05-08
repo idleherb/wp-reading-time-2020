4eec5809d6d4780eeaa800ce9cb1796d83cd2f66
idleherb <idleherb@posteo.org>
2020-05-09 01:02:13 +0200

    Update ReadingTime.test.php
    - test CI: phpunit

--- tests/ReadingTime.test.php
+++ tests/ReadingTime.test.php
@@ -7,12 +7,12 @@ class ReadingTimeTest extends WP_UnitTestCase {
 	public function test_get_reading_time_with_200_words() {
 		$text     = implode( ' ', array_fill( 0, 249, 'foo' ) );
 		$actual   = ReadingTime::get_reading_time( $text );
-		$expected = 1;
+		$expected = 0;
 		$this->assertEquals( $expected, $actual );
 	}
 
 	public function test_get_reading_time_with_4001_words() {
-		$text = implode( ' ', array_fill( 0, 4001, 'foo' ) );
+		$text     = implode( ' ', array_fill( 0, 4001, 'foo' ) );
 		$actual   = ReadingTime::get_reading_time( $text );
 		$expected = 17;
 		$this->assertEquals( $expected, $actual );
