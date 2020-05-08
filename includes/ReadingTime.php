a45eb1fdae65c8c5b85bb4bb5766b4ef7011152c
idleherb <idleherb@posteo.org>
2020-05-08 21:52:58 +0200

    Update ReadingTime.php

--- includes/ReadingTime.php
+++ includes/ReadingTime.php
@@ -6,7 +6,7 @@ class ReadingTime {
 
 	private static $speed = 250;
 
-	public static function get_reading_time( string $text ): int {
+	public static function get_reading_time( $text ) {
 		$words = explode( ' ', $text );
 		return (int) ceil( count( $words ) / self::$speed );
 	}
