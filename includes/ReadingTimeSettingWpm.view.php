c96a0252566ac727774f4d7250c56edd4f6310ed
idleherb <idleherb@posteo.org>
2020-05-20 01:02:49 +0200

    Add default wpm setting

--- includes/ReadingTimeSettingWpm.view.php
+++ includes/ReadingTimeSettingWpm.view.php
@@ -3,5 +3,5 @@
 <input
 	type='number'
 	name='reading_time_settings[reading_time_setting_wpm]'
-	value='<?php echo esc_attr( get_option( 'reading_time_settings' )['reading_time_setting_wpm'] ); ?>'
+	value='<?php echo esc_attr( get_option( 'reading_time_settings', array( 'reading_time_setting_wpm' => '250' ) )['reading_time_setting_wpm'] ); ?>'
 >
